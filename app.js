b3218743496236a1a88e619459522fd3deb13715
kaazu1224 <75646380+kazun1224@users.noreply.github.com>
2022-01-30 15:06:28 +0900

    キャンプ場の一覧ページと詳細ページの作成

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const path = require("path");
-const mongoose = require('mongoose');
-const Campground = require('./models/campground');
+const mongoose = require("mongoose");
+const Campground = require("./models/campground");
 const app = express();
 
 mongoose
@@ -20,13 +20,18 @@ app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 
 app.get("/", (req, res) => {
-  res.render('home');
+  res.render("home");
 });
 
-app.get("/makecampground", async (req, res) => {
-  const camp = new Campground({title: '私の庭', description: '気軽に安くキャンプ'})
-  await camp.save();
-  res.send(camp);
+app.get("/campgrounds", async (req, res) => {
+  const campgrounds = await Campground.find({});
+  res.render("campgrounds/index", { campgrounds });
+});
+
+app.get("/campgrounds/:id", async (req, res) => {
+  const { id } = req.params;
+  const campground = await Campground.findById(id);
+  res.render("campgrounds/show", { campground });
 });
 
 app.listen(3000, () => {
