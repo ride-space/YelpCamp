13445e4f9da939b98195eafb9d13b1c2c1fa0359
kaazu1224 <75646380+kazun1224@users.noreply.github.com>
2022-09-16 23:43:05 +0900

    ♻️

--- routes/reviews.js
+++ routes/reviews.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const router = express.Router({mergeParams: true});
+const router = express.Router({ mergeParams: true });
 const catchAsync = require("../utils/catchAsync");
 const ExpressError = require("../utils/ExpressError");
 const { reviewSchema } = require("../schemas");
