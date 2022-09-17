410c9b92c5d36f9788cc12caa27d105a5a2f25f7
kaazu1224 <75646380+kazun1224@users.noreply.github.com>
2022-09-17 12:17:34 +0900

    errorMessagesの変更とコードの修正

--- models/user.js
+++ models/user.js
@@ -1,16 +1,26 @@
 const mongoose = require("mongoose");
 const { Schema } = mongoose;
-const passportLocalMongoose = require("passport-local-mongoose")
+const passportLocalMongoose = require("passport-local-mongoose");
 
 const userSchema = new Schema({
   email: {
-    type:String,
-    require:true,
-    unique:true
+    type: String,
+    require: true,
+    unique: true,
   },
+});
 
-})
+userSchema.plugin(passportLocalMongoose, {
+  errorMessages: {
+    MissingPasswordError: "パスワードが与えられていない",
+    AttemptTooSoonError: "現在、アカウントはロックされています。後で再試行する",
+    TooManyAttemptsError: "ログイン失敗回数が多すぎてアカウントがロックされた",
+    NoSaltValueStoredError: "認証ができない。ソルト値が保存されていない",
+    IncorrectPasswordError: "パスワードまたはユーザー名が正しくない",
+    IncorrectUsernameError: "パスワードまたはユーザー名が正しくない",
+    MissingUsernameError: "ユーザー名が指定されていない",
+    UserExistsError: "そのユーザー名は既に使われています",
+  },
+});
 
-userSchema.plugin(passportLocalMongoose);
-
-module.exports =mongoose.Schema('User',userSchema);
+module.exports = mongoose.model("User", userSchema);
