0abd1ba0e43924e6d3a7e1b0d0135bc14ae9b167
kaazu1224 <75646380+kazun1224@users.noreply.github.com>
2022-09-17 12:16:46 +0900

    passportの初期化とuserのルーティングを追加

--- app.js
+++ app.js
@@ -4,19 +4,22 @@ const mongoose = require("mongoose");
 const ejsMate = require("ejs-mate");
 const methodOverride = require("method-override");
 const ExpressError = require("./utils/ExpressError");
-const session = require('express-session');
-const flash = require('connect-flash')
-
+const session = require("express-session");
+const flash = require("connect-flash");
+const passport = require("passport");
+const LocalStrategy = require("passport-local");
+const User = require("./models/user");
 
 const campgroundRoutes = require("./routes/campgrounds");
 const reviewRoutes = require("./routes/reviews");
+const userRoutes = require("./routes/users");
 
 mongoose
   .connect("mongodb://localhost:27017/yelp-camp", {
     useNewUrlParser: true,
     useUnifiedTopology: true,
-    useCreateIndex:true,
-    useFindAndModify:false
+    useCreateIndex: true,
+    useFindAndModify: false,
   })
   .then(() => {
     console.log("MongoDBコネクションOK!");
@@ -34,22 +37,27 @@ app.use(methodOverride("_method"));
 app.use(express.static(path.join(__dirname, "public")));
 
 const sessionConfig = {
-  secret: 'mySecret',
+  secret: "mySecret",
   resave: false,
   saveUninitialized: true,
   cookie: {
     httpOnly: true,
-    maxAge: 1000 * 60 * 60 *24 *7
-  }
-}
+    maxAge: 1000 * 60 * 60 * 24 * 7,
+  },
+};
 app.use(session(sessionConfig));
-app.use(flash())
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
 
-app.use((req,res,next) =>{
-  res.locals.success = req.flash('success');
-  res.locals.error = req.flash('error');
+app.use(flash());
+app.use((req, res, next) => {
+  res.locals.success = req.flash("success");
+  res.locals.error = req.flash("error");
   next();
-})
+});
 
 app.get("/", (req, res) => {
   res.render("home");
@@ -57,6 +65,7 @@ app.get("/", (req, res) => {
 
 app.use("/campgrounds", campgroundRoutes);
 app.use("/campgrounds/:id/reviews", reviewRoutes);
+app.use("/", userRoutes);
 
 app.all("*", (req, res, next) => {
   next(new ExpressError("ページが見つかりませんでした。", 404));
