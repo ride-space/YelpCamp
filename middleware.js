24f2f56558ef1c01184dd52646579b13bb99d6ad
kaazu1224 <75646380+kazun1224@users.noreply.github.com>
2022-10-03 16:32:35 +0900

    各ミドルウェアをまとめた

--- middleware.js
+++ middleware.js
@@ -1,3 +1,10 @@
+const ExpressError = require("./utils/ExpressError");
+const { campgroundSchema,reviewSchema } = require("./schemas");
+const Campground = require("./models/campground");
+
+
+
+
 module.exports.isLoggedIn = (req, res, next) => {
   if (!req.isAuthenticated()) {
     req.session.returnTo = req.originalUrl;
@@ -5,4 +12,35 @@ module.exports.isLoggedIn = (req, res, next) => {
     return res.redirect("/login");
   }
   next();
+};
+
+module.exports.validateCampground = (req, res, next) => {
+  const { error } = campgroundSchema.validate(req.body);
+  if (error) {
+    const message = error.details.map((detail) => detail.message).join(",");
+    throw new ExpressError(message, 404);
+  } else {
+    next();
+  }
+};
+
+module.exports.isAuthor = async (req, res, next) => {
+  const { id } = req.params;
+  const campground = await Campground.findById(id);
+  if (!campground.author.equals(req.user._id)) {
+    req.flash("error", "そのアクションの権限がありあません");
+    return res.redirect(`/campgrounds/${campground._id}`);
+  } else {
+    next();
+  }
+};
+
+module.exports.ValidateReview = (req, res, next) => {
+  const { error } = reviewSchema.validate(req.body);
+  if (error) {
+    const message = error.details.map((detail) => detail.message).join(",");
+    throw new ExpressError(message, 404);
+  } else {
+    next();
+  }
 };
