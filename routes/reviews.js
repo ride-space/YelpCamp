58fe7f74e708722836cced64dca2c250d14599c7
kaazu1224 <75646380+kazun1224@users.noreply.github.com>
2022-10-03 16:33:46 +0900

    ミドルウェアを移動

--- routes/reviews.js
+++ routes/reviews.js
@@ -1,20 +1,9 @@
 const express = require("express");
 const router = express.Router({ mergeParams: true });
 const catchAsync = require("../utils/catchAsync");
-const ExpressError = require("../utils/ExpressError");
-const { reviewSchema } = require("../schemas");
 const Campground = require("../models/campground");
 const Review = require("../models/review");
-
-const ValidateReview = (req, res, next) => {
-  const { error } = reviewSchema.validate(req.body);
-  if (error) {
-    const message = error.details.map((detail) => detail.message).join(",");
-    throw new ExpressError(message, 404);
-  } else {
-    next();
-  }
-};
+const { ValidateReview } = require("../middleware");
 
 router.post(
   "/",
