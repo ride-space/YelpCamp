004f30da7a9f318be400ba486e25700a1805f1dc
kaazu1224 <75646380+kazun1224@users.noreply.github.com>
2022-10-03 17:56:23 +0900

    campgroundのcontrollerを作成

---
+++ controllers/campgrounds.js
@@ -0,0 +1,65 @@
+const Campground = require("../models/campground");
+
+module.exports.index = async (req, res) => {
+  const campgrounds = await Campground.find({});
+  res.render("campgrounds/index", { campgrounds });
+};
+
+module.exports.renderNewForm = (req, res) => {
+  res.render("campgrounds/new");
+};
+
+module.exports.showCampground = async (req, res) => {
+  const { id } = req.params;
+  const campground = await Campground.findById(id)
+    .populate({
+      path: "reviews",
+      populate: {
+        path: "author",
+      },
+    })
+    .populate("author");
+  if (!campground) {
+    req.flash("error", "キャンプ場はみつかりませんでした。");
+    return res.redirect("/campgrounds");
+  }
+  res.render("campgrounds/show", { campground });
+};
+
+module.exports.createCampground = async (req, res, next) => {
+  const campground = await new Campground(req.body.campground);
+  campground.author = req.user._id;
+  await campground.save();
+  req.flash("success", "新しいキャンプ場を登録しました");
+  res.redirect(`/campgrounds/${campground._id}`);
+};
+
+module.exports.renderEditForm = async (req, res) => {
+  const { id } = req.params;
+  const campground = await Campground.findById(id);
+
+  if (!campground) {
+    req.flash("error", "キャンプ場はみつかりませんでした。");
+    return res.redirect("/campgrounds");
+  }
+
+  res.render("campgrounds/edit", { campground });
+};
+
+module.exports.updateCampground = async (req, res) => {
+  const { id } = req.params;
+  const campground = await Campground.findByIdAndUpdate(
+    id,
+    { ...req.body.campground },
+    { new: true }
+  );
+  req.flash("success", "キャンプ場を更新しました。");
+  res.redirect(`/campgrounds/${campground._id}`);
+};
+
+module.exports.deleteCampground = async (req, res) => {
+  const { id } = req.params;
+  await Campground.findByIdAndDelete(id);
+  req.flash("success", "キャンプ場を削除しました。");
+  res.redirect("/campgrounds");
+};
