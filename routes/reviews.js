9772da384ce4c2bf2c5e58c762f7241cb54948d3
kaazu1224 <75646380+kazun1224@users.noreply.github.com>
2022-10-03 17:33:47 +0900

    ミドルウェアを適用

--- routes/reviews.js
+++ routes/reviews.js
@@ -3,15 +3,17 @@ const router = express.Router({ mergeParams: true });
 const catchAsync = require("../utils/catchAsync");
 const Campground = require("../models/campground");
 const Review = require("../models/review");
-const { ValidateReview } = require("../middleware");
+const { ValidateReview ,isLoggedIn,isReviewAuthor} = require("../middleware");
 
 router.post(
   "/",
+  isLoggedIn,
   ValidateReview,
   catchAsync(async (req, res) => {
     const { id } = req.params;
     const campground = await Campground.findById(id);
     const review = new Review(req.body.review);
+    review.author = req.user._id
     campground.reviews.push(review);
     await review.save();
     await campground.save();
@@ -22,6 +24,8 @@ router.post(
 
 router.delete(
   "/:reviewId",
+  isLoggedIn,
+  isReviewAuthor,
   catchAsync(async (req, res) => {
     const { id, reviewId } = req.params;
     await Campground.findByIdAndUpdate(id, { $pull: { reviews: reviewId } });
