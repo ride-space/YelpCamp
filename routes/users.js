d4a092e4f026061c8ea32a1538c1aa8691cc57e9
kaazu1224 <75646380+kazun1224@users.noreply.github.com>
2022-10-04 12:33:59 +0900

    ♻️

--- routes/users.js
+++ routes/users.js
@@ -3,19 +3,18 @@ const router = express.Router({ mergeParams: true });
 const passport = require("passport");
 const users = require("../controllers/users");
 
-router.get("/register", users.renderRegister);
-router.post("/register", users.register);
+router.route("/register").get(users.renderRegister).post(users.register);
 
-router.get("/login", users.renderLogin);
-
-router.post(
-  "/login",
-  passport.authenticate("local", {
-    failureFlash: true,
-    failureRedirect: "/login",
-  }),
-  users.login
-);
+router
+  .route("/login")
+  .get(users.renderLogin)
+  .post(
+    passport.authenticate("local", {
+      failureFlash: true,
+      failureRedirect: "/login",
+    }),
+    users.login
+  );
 
 router.post("/logout", users.logout);
 module.exports = router;
