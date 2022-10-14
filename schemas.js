a6e010453fa2729cc5a546b28547b50a6e915423
kaazu1224 <75646380+kazun1224@users.noreply.github.com>
2022-10-14 23:10:54 +0900

    campgroundSchemaにdeleteImagesを追加

--- schemas.js
+++ schemas.js
@@ -11,6 +11,7 @@ module.exports.campgroundSchema = Joi.object({
     description: Joi.string().required(),
     location: Joi.string().required(),
   }).required(),
+  deleteImages: Joi.array()
 });
 
 module.exports.reviewSchema = Joi.object({
