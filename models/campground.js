4006c37ce8297492b9f95a1b6781a6304d566489
kaazu1224 <75646380+kazun1224@users.noreply.github.com>
2022-10-15 14:43:44 +0900

    Schemaにgeometryを追加

--- models/campground.js
+++ models/campground.js
@@ -19,6 +19,17 @@ const campgroundSchema = new Schema({
   images: [
     imageSchema
   ],
+  geometry: {
+    type: {
+      type: String, // Don't do `{ location: { type: String } }`
+      enum: ['Point'], // 'location.type' must be 'Point'
+      required: true
+    },
+    coordinates: {
+      type: [Number],
+      required: true
+    }
+  },
   price: Number,
   description: String,
   location: String,
